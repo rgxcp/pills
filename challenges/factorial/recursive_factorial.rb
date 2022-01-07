66e70f77c4369956688643f1e6abcf33b7d3a6db
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-08 06:31:13 +0700

    test: add test for recursive factorial solution in ruby
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- challenges/factorial/recursive_factorial.rb
+++ challenges/factorial/recursive_factorial.rb
@@ -1,14 +1,7 @@
-def factorial(n)
+def recursive_factorial(n)
   if n == 0
     return 1
   end
 
-  n * factorial(n - 1)
+  n * recursive_factorial(n - 1)
 end
-
-puts factorial(0)
-puts factorial(1)
-puts factorial(2)
-puts factorial(3)
-puts factorial(4)
-puts factorial(5)

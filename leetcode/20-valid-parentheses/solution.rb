41d1f6761c2adb0974461b8f071b95be22fff231
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-01 16:36:13 +0700

    LC-20: is_valid returns true when given input '()'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ leetcode/20-valid-parentheses/solution.rb
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+# @param {String} s
+# @return {Boolean}
+def is_valid(s)
+  s == "()"
+end
---
+++ leetcode/20-valid-parentheses/solution_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+require_relative "solution"
+
+RSpec.describe "is_valid" do
+  context "when given input '()'" do
+    it "returns true" do
+      expect(is_valid("()")).to be true
+    end
+  end
+end
